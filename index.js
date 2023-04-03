f12839196f70c831f5f1d1c2788566414c4d6887
harsh-webk22 <aggarwalharsh912@gmail.com>
2023-04-03 23:34:28 +0530

    Added ssl

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
 const express = require('express');
 const mysql= require('mysql')
 const app = express();
+const http = require('http');
+const https = require('https')
 const cors = require('cors')
-
+const path = require("path");
+const fs = require('fs')
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors())
 
-app.use(express.static("public"))
+app.use(express.static(__dirname+"/public"))
+
+
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/thefitness365.life/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/thefitness365.life/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/thefitness365.life/chain.pem', 'utf8');
+
+const credentials = {
+        key: privateKey,
+        cert: certificate,
+        ca: ca
+};
 
 app.get('/hey', (req, res)=>{
     res.json({
         message: "heyyy"
     })
+   //  res.sendFile(path.join(__dirname, './public/index.html'));
 })
 
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "",
+    password: "password",
     database:"gym"
   });
-  
+
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
@@ -44,8 +60,6 @@ app.get('/getUser', (req, res)=>{
 // //get due payment list
 app.get('/duePayment', (req, res)=>{
     const today =new Date().toISOString().slice(0, 10);
-    console.log(today)
-    console.log(today < new Date("2024-03-16T14:44:38.122Z"))
     const query = `SELECT * FROM customers WHERE expiry_date < '${today}'`;
     con.query(query, (error, results, fields) => {
         if (error) {
@@ -54,47 +68,61 @@ app.get('/duePayment', (req, res)=>{
             return;
         }
         console.log(results)
-        res.json(results); 
+        res.json(results);
     });
 })
 
 // //get all user
 app.get('/allUser', (req, res)=>{
     con.query("SELECT * FROM customers", function (err, result, fields) {
-        if (err) throw err;
+        if (err) {
+            return res.json([]);
+        };
         res.json(result);
-    });    
+    });
 })
+
 
 
 // //create user
 app.post('/customers', (req, res) => {
-    console.log(req.body)
     const { name, number,fees, dob,  age, gender, expiry_date, joining_date } = req.body;
-  
+
     // insert new customer into customers table
-    const sql = 'INSERT INTO customers (name, number, age, fees, dob, gender, expiry_date, joining_date) VALUES (?, ?, ?, ?, ?, ?)';
+    const sql = 'INSERT INTO customers (name, number, age, fees, dob, gender, expiry_date, joining_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
     con.query(sql, [name, number, age, fees, dob, gender, expiry_date, joining_date], (err, result) => {
-      if (err) throw err;
-      console.log(result);
+      if (err) {
+        return res.staus(400).json({
+            message:"Error in saving data"
+        })
+      };
       res.status(200).send('Customer added successfully!');
     });
   });
 
 // //update user
 app.post('/updateUser', (req, res)=>{
-    console.log(req.body)
     var {number , expiry} = req.body
     // expiry = new Date(expiry).toISOString().slice(0, 10);
     var sql = `UPDATE customers SET expiry_date = ${ expiry} WHERE number = ${number}`;
     con.query(sql, function (err, result) {
-        if (err) throw err;
-            console.log(result.affectedRows + " record(s) updated");
+        if (err) {
+            return res.status(400).json({
+                message:"Error in updating customers"
+            })
+        }
             res.send("customer updated")
         });
 })
 
 
-app.listen(80, ()=>{
-    console.log("app is running")
-})
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+    console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+    console.log('HTTPS Server running on port 443');
+});
--- public/script.js
+++ public/script.js
@@ -1,120 +1,165 @@
-var randon = async () => {
-    var ans = await fetch("/hey");
-    ans = await ans.json();
-    console.log(ans)
-}
+var table = document.querySelector(".table");
+var func = async () => {
+    try{
+        var users = await fetch("/getUser");
+        users = await users.json();
+        users.map((item) => {
+            var tr = document.createElement("tr");
 
-randon();
-
-var table = document.querySelector('.table')
-var func = async ()=>{
-    var users = await fetch("/getUser")
-    users = await users.json()
-    users.map((item)=>{
-        var tr = document.createElement('tr')
-
-        let props = Object.keys(item)
-        props.forEach((property)=>{
-            if(property == "joining_date" || property== "expiry_date"){
-                console.log("inside")
-                var inputDate = new Date(item[property])
-                const day = inputDate.getDate().toString().padStart(2, '0');
-                const month = (inputDate.getMonth() + 1).toString().padStart(2, '0');
+            let props = Object.keys(item);
+            props.forEach((property) => {
+            if (
+                property == "joining_date" ||
+                property == "expiry_date" ||
+                property == "dob"
+            ) {
+                console.log("inside");
+                var inputDate = new Date(item[property]);
+                const day = inputDate.getDate().toString().padStart(2, "0");
+                const month = (inputDate.getMonth() + 1).toString().padStart(2, "0");
                 const year = inputDate.getFullYear().toString().slice(2);
                 const outputDate = `${day}/${month}/${year}`;
-                date = outputDate
-                var td = document.createElement('td')
-                var h4 = document.createElement('h4')
-                h4.innerText = date
+                var date = outputDate;
+                var td = document.createElement("td");
+                var h4 = document.createElement("h4");
+                h4.innerText = date;
                 td.appendChild(h4);
                 tr.appendChild(td);
-            } else{
-                var td = document.createElement('td')
-                var h4 = document.createElement('h4')
-                h4.innerText = item[property]
+            } else {
+                var td = document.createElement("td");
+                var h4 = document.createElement("h4");
+                h4.innerText = item[property];
                 td.appendChild(h4);
                 tr.appendChild(td);
             }
-        })
-        table.appendChild(tr)
-    })
-}
-if (table) func()
 
+            });
+            // 
+            table.appendChild(tr);
+        });
+    }catch(err){
+        alert("error in retrieving data")
+    }
+  
+};
+if (table) func();
 
-var dueTable = document.querySelector('.due')
-var duePayment = async ()=>{
-    var users = await fetch("/duePayment")
-    users = await users.json()
-    console.log(users)
-    users.map((item)=>{
-        var tr = document.createElement('tr')
-        let props = Object.keys(item)
-        props.forEach((property)=>{
-            if(property == "joining_date" || property== "expiry_date"){
-                console.log("inside")
-                var inputDate = new Date(item[property])
-                const day = inputDate.getDate().toString().padStart(2, '0');
-                const month = (inputDate.getMonth() + 1).toString().padStart(2, '0');
+var dueTable = document.querySelector(".due");
+var duePayment = async () => {
+    try{
+        var users = await fetch("/duePayment");
+        users = await users.json();
+        console.log(users);
+        users.map((item) => {
+            var tr = document.createElement("tr");
+            let props = Object.keys(item);
+            props.forEach((property) => {
+            if (
+                property == "joining_date" ||
+                property == "expiry_date" ||
+                property == "dob"
+            ) {
+                console.log("inside");
+                var inputDate = new Date(item[property]);
+                const day = inputDate.getDate().toString().padStart(2, "0");
+                const month = (inputDate.getMonth() + 1).toString().padStart(2, "0");
                 const year = inputDate.getFullYear().toString().slice(2);
                 const outputDate = `${day}/${month}/${year}`;
-                date = outputDate
-                var td = document.createElement('td')
-                var h4 = document.createElement('h4')
-                h4.innerText = date
+                var date = outputDate;
+                var td = document.createElement("td");
+                var h4 = document.createElement("h4");
+                h4.innerText = date;
                 td.appendChild(h4);
                 tr.appendChild(td);
-            } else{
-                var td = document.createElement('td')
-                var h4 = document.createElement('h4')
-                h4.innerText = item[property]
+            } else {
+                var td = document.createElement("td");
+                var h4 = document.createElement("h4");
+                h4.innerText = item[property];
                 td.appendChild(h4);
                 tr.appendChild(td);
             }
-        })
-        dueTable.appendChild(tr)
-    })
-}
+            });
+            var button12 = document.createElement('button');
+            button12.innerHTML = 'send message';
+            tr.appendChild(button12);
+
+            dueTable.appendChild(tr);
+        });
+    }catch(err){
+        alert("Error in retreiving data!")
+    }
+};
+
+if (dueTable) duePayment();
 
 
-if(dueTable) duePayment()
 
-
-
-var allTable = document.querySelector('.alldata')
-var alldata = async ()=>{
-    var users = await fetch("/allUser")
-    users = await users.json()
-    console.log(users)
-    users.map((item)=>{
-        var tr = document.createElement('tr')
-        let props = Object.keys(item)
-        props.forEach((property)=>{
-            if(property == "joining_date" || property== "expiry_date"){
-                console.log("inside")
-                var inputDate = new Date(item[property])
-                const day = inputDate.getDate().toString().padStart(2, '0');
-                const month = (inputDate.getMonth() + 1).toString().padStart(2, '0');
+let array = [];
+var allTable = document.querySelector(".alldata");
+var alldata = async () => {
+    try{
+        var users = await fetch("/allUser");
+        array = await users.json();
+        console.log(users);
+        array.map((item) => {
+            var tr = document.createElement("tr");
+            let props = Object.keys(item);
+            props.forEach((property) => {
+            if (
+                property == "joining_date" ||
+                property == "expiry_date" ||
+                property == "dob"
+            ) {
+                console.log("inside");
+                var inputDate = new Date(item[property]);
+                const day = inputDate.getDate().toString().padStart(2, "0");
+                const month = (inputDate.getMonth() + 1).toString().padStart(2, "0");
                 const year = inputDate.getFullYear().toString().slice(2);
                 const outputDate = `${day}/${month}/${year}`;
-                date = outputDate
-                var td = document.createElement('td')
-                var h4 = document.createElement('h4')
-                h4.innerText = date
+                var date = outputDate;
+                var td = document.createElement("td");
+                var h4 = document.createElement("h4");
+                h4.innerText = date;
                 td.appendChild(h4);
                 tr.appendChild(td);
-            } else{
-                var td = document.createElement('td')
-                var h4 = document.createElement('h4')
-                h4.innerText = item[property]
+            } else {
+                var td = document.createElement("td");
+                var h4 = document.createElement("h4");
+                h4.innerText = item[property];
                 td.appendChild(h4);
                 tr.appendChild(td);
             }
-        })
-        allTable.appendChild(tr)
-    })
+            });
+            allTable.appendChild(tr);
+        });
+    }catch(err){
+
+    }
+  
+};
+
+if (allTable) alldata();
+
+
+
+const searchfun = () => {
+  let filter = document.getElementById("myinput").value;
+  let mytable = document.getElementById("myTable");
+  let tr = mytable.getElementsByTagName("tr");
+
+  for (var i = 0; i < tr.length; i++) {
+    let td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      let textvlaue = td.textContent || td.innerHTML;
+      if (textvlaue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
 }
 
 
-if(allTable) alldata()
+
 
